4c4d4804e59f03a26ac4437e371764619837d6b2
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-11-14 13:56:14 +0200

    Update lab_3_task_2.py

--- lab_3_task_2.py
+++ lab_3_task_2.py
@@ -2,7 +2,7 @@ from lab_3_task_1 import g, h, e, k
 from math import sin, tan, cos, pi, sqrt
 h = 100
 a = pi/3
-B = 30
+B = 30*pi/180
 V = sqrt((g*h*(tan(B)**2))/(2*(cos(a)**2)*(1-tan(B)*tan(a))))
 print(V)
 T = 200
