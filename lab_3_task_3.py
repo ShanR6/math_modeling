fbe39cbf05f2eb4becfcc7b1ea2966a8c37650b4
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-11-14 13:51:48 +0200

    Update lab_3_task_3.py

--- lab_3_task_3.py
+++ lab_3_task_3.py
@@ -3,10 +3,12 @@ import numpy as np
 x0 = 2
 y0 = 3
 V0 = 4
-t = range(0,6)
-T = np.array(t)
-x = x0 + V0*T
-y = y0 + V0*T - (g*(T**2))/2
-print('t -',t)
-print('x -',x)
-print('y -',y)
+t = np.linspace(0,5,100)
+x = x0 + V0*t
+y = y0 + V0*t - (g*(t**2))/2
+A = np.zeros((100,3))
+for i in range(100):
+  A[i,0]=t[i]
+  A[i,1]=x[i]
+  A[i,2]=y[i]
+print(A)
