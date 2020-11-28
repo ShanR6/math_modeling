34ca05a29bdc4eb847016993739e4cc44f499b13
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-11-28 14:55:37 +0200

    Add files via upload

---
+++ lab_4_task_1.py
@@ -0,0 +1,14 @@
+a1 = [1,2,3]
+def arf(a): 
+  b=a/len(a1)
+  return b 
+qarf = arf(a=sum(a1))
+print(qarf)
+
+#----------------------
+def arf(a1): 
+  a = sum(a1)
+  b=a/len(a1)
+  print(b) 
+a1 = [1,2,3]
+arf(a1)
