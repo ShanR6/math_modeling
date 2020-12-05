78a60c4f7a37a093fe9cbcd6a652e1e3153624f0
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-12-05 12:13:46 +0300

    Update lab_4_task_3.py

--- lab_4_task_3.py
+++ lab_4_task_3.py
@@ -4,3 +4,14 @@ def high(m=10,v=5,h=3,g=9.81):
   E = E1 + E2
   print(E)
 high()
+#------------------------------
+m = int(input('Введите массу тела:'))
+v = int(input('Введите скорость тела:'))
+h = int(input('Введите высоту полёта тела:'))
+g = 9.81
+def high(m,v,h,g):
+  E1 = (m*v**2)/2
+  E2 = m*g*h
+  E = E1 + E2
+  print(E)
+high(m,v,h,g)
