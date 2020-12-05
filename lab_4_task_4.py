2172d1e4ca4b2db27c103bf51ac4e57655827378
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-12-05 12:11:06 +0300

    Update lab_4_task_4.py

--- lab_4_task_4.py
+++ lab_4_task_4.py
@@ -6,3 +6,15 @@ def func(a=2,b=15):
     y=x*x
     print(y)
 func()
+#----------------------------------
+print('Функция y=x*x')
+def func(a=2,b=15):
+  N1 = int(input('1 точка:'))
+  N2 = int(input('2 точка:'))
+  if a<N1<b:
+    y1=N1*N1
+    print('В первой точке:',y1)
+  if a<N2<b:
+    y2=N2*N2
+    print('Во второй точке:',y2)
+func()
