d654c489c9a39bcde328e3baaae52bbc2d2da9dd
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-12-05 13:51:49 +0200

    Update lab_4_task_2.py

--- lab_4_task_2.py
+++ lab_4_task_2.py
@@ -1,7 +1,10 @@
 import numpy as np
-a = [1,2,3,4]
+a = [1,2,3,4,5]
 a1 = np.array(a)
 def mul(a1):
-  b = a1[0]*a1[1]*a1[2]*a1[3]
+  b = 1
+  for i in range(0,len(a1)):
+    A = b * a1[i]
+    b = A
   print(b)
 mul(a1)
