5835e42de949d6ec813ef4b46fad160c701cc042
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-12-12 00:41:15 +0300

    Create lab_4_task_2_DOP.py

---
+++ lab_4_task_2_DOP.py
@@ -0,0 +1,20 @@
+def fib(n):
+  a0 = 0
+  a1 = 1
+  i = 0
+  while i < n-2:
+    A = a0 + a1
+    a0=a1-1
+    a1 = A
+    i = i+1
+    print(A)
+fib(5)
+#------------------------------
+def fib(n):
+  a0 = 0
+  a1 = 1
+  while n>0:
+    B=a0,a1=a1,a0+a1
+    n = n-1
+    print(B)
+fib(5)
