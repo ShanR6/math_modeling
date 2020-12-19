537dd9adeafb81107c96d959f171c420315843e9
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-12-19 15:16:51 +0300

    Update lab_6_task_4.py

--- lab_6_task_4.py
+++ lab_6_task_4.py
@@ -1,28 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sqrt,pi,sin
-w='логарифмическая спираль(l),архимедова спираль(ar), спираль жезл(j), роза(r)'
-def ris(w,e=1,b=3,f=4,k=5):
-  if w=='l':
-    x=np.arange(0,8*pi,0.01)
-    y=np.arange(0,8*pi,0.01)
-    X, Y = np.meshgrid(x, y)
-    r=e**(b*f)
-  elif w=='ar':
-    x=np.arange(0,8*pi,0.01)
-    y=np.arange(0,8*pi,0.01)
-    X, Y = np.meshgrid(x, y)
-    r=k*f
-  elif w=='j':
-    x=np.arange(0.01,8*pi,0.01)
-    y=np.arange(0.01,8*pi,0.01)
-    X, Y = np.meshgrid(x, y)
-    r=k/sqrt(f)
-  elif w=='r':
-    x=np.arange(0,8*pi,0.01)
-    y=np.arange(0,8*pi,0.01)
-    X, Y = np.meshgrid(x, y)
-    r=sin(k*f)
-  plt.contour(X,Y,r,levels=[0])
+def ris(s=3,b=1,k=2):
+  '''s=1(логарифмическая спираль),s=2(архимедова спираль),s=3(спираль "жезл"),s=4(роза)'''
+  if s==1:
+    phi=np.arange(0,8*np.pi,0.01)
+    r=np.exp(b*phi)
+  elif s==2:
+    phi=np.arange(0,8*np.pi,0.01)
+    r=k*phi
+  elif s==3:
+    phi=np.arange(0.01,8*np.pi,0.01)
+    r=k/np.sqrt(phi)
+  elif s==4:
+    phi=np.arange(0,8*np.pi,0.01)
+    r=np.sin(k*phi)
+  x=r*np.cos(phi)
+  y=r*np.sin(phi)
+  plt.plot(x,y)
   plt.show()
-ris(w='l')
+ris()
