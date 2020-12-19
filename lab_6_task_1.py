1883b563b31b26e420c9cfd57e554186a5ef80ae
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-12-19 12:02:44 +0300

    Update lab_6_task_1.py

--- lab_6_task_1.py
+++ lab_6_task_1.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x = [1,1,5,5,1]
-y = [1,5,5,1,1]
-plt.plot(x,y, color='k', label='lunchte', marker='o',ms=5)
+plt.plot([1,1,5,5,1],[1,5,5,1,1],color='k', label='lunchte', marker='o',ms=5)
 plt.axis('equal')
 plt.show()
