e580f616300b9acc6b3db91ff5181c72737b2085
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2020-12-19 12:27:23 +0300

    Update lab_6_task_3.py

--- lab_6_task_3.py
+++ lab_6_task_3.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
-f = input('Окружность(o) или эллипс(e):')
+f='Окружность(o) или эллипс(e)'
 
-
-def ris(f,rad=50,xa=-100,xb=100,N=5):
+def ris(f,rad=50,xa=-100,xb=100,N=5,a=5,b=4):
   x = np.arange(xa,xb,N)
   y = np.arange(xa,xb,N)
   X, Y = np.meshgrid(x,y)
   if f == 'o':
     fxy = X**2 + Y**2
     plt.title('Окружность')
+    plt.axis('equal')
   elif f == 'e':
-    X = 
+    fxy=X+Y
     plt.title('Эллипс')
-  
   plt.contour(X,Y,fxy,levels=[rad**2])
-  plt.axis('equal')
   plt.show()
-ris(f)
+ris(f='o')
