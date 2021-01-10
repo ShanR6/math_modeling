e6c2622a20f18dc3d0a3ea68f1513786b91365d1
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-11 00:59:38 +0300

    Update lab_7_task_1.py

--- lab_7_task_1.py
+++ lab_7_task_1.py
@@ -16,3 +16,20 @@ def ris(f,R):
   return x,y
 
 print(ris(f='a',R=5))
+#--------------------------------
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ris(f,R):
+  if f=='c':
+	  t = np.arange(0, 2*np.pi, 0.01)
+	  x = R*(t-np.sin(t))
+	  y = R*(1-np.cos(t))
+  elif f=='a':
+	  t = np.arange(0, 2*np.pi, 0.01)
+	  x = R*np.cos(t)**3
+	  y = R*np.sin(t)**3
+  plt.plot(x,y,ls='-',lw=3)
+  plt.axis('equal')
+  plt.show()
+print(ris(f='c',R=4))
