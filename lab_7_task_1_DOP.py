fcae2e21dd67cd6c794eb61c384ea5dc8bbe1178
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-12 01:58:41 +0300

    Update lab_7_task_1_DOP.py

--- lab_7_task_1_DOP.py
+++ lab_7_task_1_DOP.py
@@ -26,3 +26,29 @@ frames=100,
 interval=30
 )
 ani.save('lab_7_task_1_DOP.gif')
+#-------------------------------
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+fig,ax=plt.subplots()
+move,=plt.plot([],[],'-',color='r',label='Frak')
+R=4
+plt.axis('equal')
+ax.set_xlim(-10,10)
+ax.set_ylim(-10,10)
+def update(i):
+  t=0.1*i
+  x1=R*(t-np.sin(t)**3)
+  y1=R*(1-np.cos(t)**3)
+  x2=(R/4)*np.cos(t)**3
+  y2=(R/4)*np.sin(t)**3
+  move.set_data(x1,y1)
+  move.set_data(x2,y2)
+
+ani=FuncAnimation(fig,
+update,
+frames=100,
+interval=30
+)
+ani.save('lab_7_task_1_DOP.gif')
