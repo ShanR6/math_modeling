95bf13f580db64f1684eefc2b2640c10072f3ec0
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-12 01:44:12 +0300

    Update lab_7_task_2.py

--- lab_7_task_2.py
+++ lab_7_task_2.py
@@ -26,3 +26,32 @@ frames=100,
 interval=30
 )
 ani.save('lab_7_task_2.gif')
+#-----------------------------------
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots() 
+ball, = plt.plot([], [], '-', color='r', label='Ball')
+alpha=np.arange(0,2*np.pi,0.1)
+R=3
+xdata, ydata = [], []
+t=np.arange(0,2*np.pi,0.1)
+edge=10
+plt.axis('equal')
+ax.set_xlim(-edge,edge)
+ax.set_ylim(-edge,edge)
+R=3
+def update(frame):
+  R=3
+  xdata.append(R*np.cos(frame))
+  ydata.append(R*np.sin(frame))
+  ball.set_data(xdata,ydata)
+  return ball,
+  
+ani=FuncAnimation(fig,
+update,
+frames=100,
+interval=30
+)
+ani.save('lab_7_task_2.gif')
