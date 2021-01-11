54724e119dd29fc5ba441e45cce500b47c183ab7
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-11 12:14:08 +0300

    Update lab_7_task_3.py

--- lab_7_task_3.py
+++ lab_7_task_3.py
@@ -4,7 +4,7 @@ from matplotlib.animation import FuncAnimation
 def ris(f):
   if f=='b':
     fig, ax = plt.subplots()
-    ball, = plt.plot([], [], 'o', color='r', label='Ball')
+    babka, = plt.plot([], [], '-', color='r', label='Babochka')
 
     def babochka(time):
       alpha = np.arange(0,12*np.pi,0.1)
@@ -17,7 +17,7 @@ def ris(f):
     ax.set_ylim(-5,5)
 
     def animate(i):
-      ball.set_data(babochka(time=i))
+      babka.set_data(babochka(time=i))
     ani = FuncAnimation(fig,
     animate,
     frames=100,
@@ -26,7 +26,7 @@ def ris(f):
     ani.save('lab_7_task_3.gif')
   elif f=='h':
     fig, ax = plt.subplots()
-    ball, = plt.plot([], [], 'o', color='r', label='Ball')
+    ser, = plt.plot([], [], '-', color='r', label='Hearth')
 
     def heart(time):
       alpha = (0,2*np.pi,0.1)
@@ -39,7 +39,7 @@ def ris(f):
     ax.set_ylim(-20,20)
 
     def animate(i):
-      ball.set_data(heart(time=i))
+      ser.set_data(heart(time=i))
     ani = FuncAnimation(fig,
     animate,
     frames=100,
@@ -47,3 +47,54 @@ def ris(f):
     )
     ani.save('lab_7_task_3.gif')
 print(ris(f='b'))
+#---------------------------------------------
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+def ris(f):
+  if f=='b':
+    fig, ax = plt.subplots()
+    babka, = plt.plot([], [], '-', color='r', label='Babochka')
+    xdata,ydata=[],[]
+    ax.set_xlim(-5,5)
+    ax.set_ylim(-5,5)
+
+    def update(t):
+      t = np.arange(0,12*np.pi,0.1)
+      x = np.sin(t)*(np.exp(np.cos(t))-2*np.cos(4*t)+np.sin(t/12)**5)
+      y = np.cos(t)*(np.exp(np.cos(t))-2*np.cos(4*t)+np.sin(t/12)**5)
+
+      xdata.append(x)
+      ydata.append(y)
+      babka.set_data(xdata,ydata)
+      return babka,
+
+    ani = FuncAnimation(fig,
+    update,
+    frames=100,
+    interval=30
+    )
+    ani.save('lab_7_task_3.gif')
+  elif f=='h':
+    fig, ax = plt.subplots()
+    ser, = plt.plot([], [], '-', color='r', label='Hearth')
+    xdata,ydata=[],[]
+    ax.set_xlim(-20,20)
+    ax.set_ylim(-20,20)
+
+    def update(i):
+      t = (0,2*np.pi,0.1)
+      x = 16*np.sin(t)**3
+      y = 13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t)
+
+      xdata.append(x)
+      ydata.append(y)
+      ser.set_data(xdata,ydata)
+      return ser,
+    ani = FuncAnimation(fig,
+    update,
+    frames=100,
+    interval=30,
+    )
+    ani.save('lab_7_task_3.gif')
+print(ris(f='h'))
