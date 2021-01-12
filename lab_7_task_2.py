8dd2297be748273be7920ad057237ab69613b049
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-12 11:20:24 +0300

    Update lab_7_task_2.py

--- lab_7_task_2.py
+++ lab_7_task_2.py
@@ -1,56 +1,26 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt 
+import numpy as np 
 from matplotlib.animation import FuncAnimation
 
-fig, ax = plt.subplots() 
-ball, = plt.plot([], [], 'o', color='r', label='Ball')
-alpha=np.arange(0,2*np.pi,0.1)
-xdata, ydata = [], []
-def circle(R,time):
-  x=R*np.cos(alpha)
-  y=R*np.sin(alpha)
+fig, ax = plt.subplots()
+circle, = plt.plot([], [], '-', color='r', label='Circle')
+
+def circle_move(phi,a,t):
+  alpha = a * (np.pi/180) * t
+  R = alpha * t
+  x = R*np.cos(phi)
+  y = R*np.sin(phi)
   return x,y
-edge=3
-plt.axis('equal')
-ax.set_xlim(-edge,edge)
-ax.set_ylim(-edge,edge)
 
-def update(frame):
-  xdata.append(frame)
-  ydata.append(np.sin(frame))
-  def animate(i):
-    ball.set_data(xdata,ydata,(circle(time=i,R=alpha*i)))
-ani=FuncAnimation(fig,
-update,
-frames=100,
-interval=30
-)
-ani.save('lab_7_task_2.gif')
-#-----------------------------------
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.animation import FuncAnimation
-
-fig, ax = plt.subplots() 
-ball, = plt.plot([], [], '-', color='r', label='Ball')
-alpha=np.arange(0,2*np.pi,0.1)
-R=3
-xdata, ydata = [], []
-t=np.arange(0,2*np.pi,0.1)
-edge=10
+edge=50
 plt.axis('equal')
-ax.set_xlim(-edge,edge)
-ax.set_ylim(-edge,edge)
-R=3
-def update(frame):
-  R=3
-  xdata.append(R*np.cos(frame))
-  ydata.append(R*np.sin(frame))
-  ball.set_data(xdata,ydata)
-  return ball,
-  
-ani=FuncAnimation(fig,
-update,
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
+
+def animate(i):
+  circle.set_data(circle_move(a=1, phi=np.linspace(0, 2*np.pi, 100), t=i))
+ani = FuncAnimation(fig,
+animate,
 frames=100,
 interval=30
 )
