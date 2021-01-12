7101c0823cef2fc947cc4c7091495e3d7b946c6f
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-12 13:52:21 +0200

    Update lab_7_task_1_DOP.py

--- lab_7_task_1_DOP.py
+++ lab_7_task_1_DOP.py
@@ -3,52 +3,35 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 fig,ax=plt.subplots()
-move,=plt.plot([],[],'o',color='r',label='Frak')
-
-def first(R,time):
-  t=np.arange(-2*np.pi,2*np.pi,0.1)
-  x=R*(t-np.sin(t))
-  y=R*(1-np.cos(t))
-  return x,y
-def second(R,time):
-  t=np.arange(-2*np.pi,2*np.pi,0.1)
-  x=(R/4)*np.cos(t)**3
-  y=(R/4)*np.sin(t)**3
-  return x,y
-plt.axis('equal')
-ax.set_xlim(-3,3)
-ax.set_ylim(-3,3)
-def animate(i):
-  move.set_data(first(time=i,R=5),second(time=i,R=5))
-ani=FuncAnimation(fig,
-animate,
-frames=100,
-interval=30
-)
-ani.save('lab_7_task_1_DOP.gif')
-#-------------------------------
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.animation import FuncAnimation
-
-fig,ax=plt.subplots()
 move,=plt.plot([],[],'-',color='r',label='Frak')
-R=4
-plt.axis('equal')
-ax.set_xlim(-10,10)
-ax.set_ylim(-10,10)
-def update(i):
-  t=0.1*i
+point,=plt.plot([],[],'o',color='r')
+circle,=plt.plot([],[],'-',color='r')
+def ne_circle_move(R,t):
   x1=R*(t-np.sin(t))
   y1=R*(1-np.cos(t))
-  x2=(R/4)*np.cos(t)**3
-  y2=(R/4)*np.sin(t)**3
-  move.set_data(x1,y1)
-  move.set_data(x2,y2)
+  return x1,y1
+def circle_move(R,vx0,vy0,t):
+  x0=vx0*t
+  y0=vy0*t
+  alpha = np.arange(0, 2*np.pi, 0.1)
+  x2=x0+R*np.cos(alpha)
+  y2=y0+R*np.sin(alpha)
+  return x2,y2
+xdata,ydata=[],[]
+edge=20
+plt.axis('equal')
+ax.set_xlim(-2*edge,2*edge)
+ax.set_ylim(-edge,edge)
 
-ani=FuncAnimation(fig,
+def update(i):
+  xdata.append(ne_circle_move(R=2,t=i)[0])
+  ydata.append(ne_circle_move(R=2,t=i)[1])
+  move.set_data(xdata,ydata)
+  point.set_data(ne_circle_move(R=2,t=i)[0],ne_circle_move(R=2,t=i)[1])
+  circle.set_data(circle_move(R=4.5,vx0=1,vy0=0,t=i))
+ani = FuncAnimation(fig,
 update,
-frames=100,
+frames=np.arange(0,4*np.pi,0.1),
 interval=30
 )
 ani.save('lab_7_task_1_DOP.gif')
