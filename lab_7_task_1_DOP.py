f6a4b5f089430b296448ca056c537d211c348d64
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-16 13:14:09 +0200

    Update lab_7_task_1_DOP.py

--- lab_7_task_1_DOP.py
+++ lab_7_task_1_DOP.py
@@ -6,32 +6,38 @@ fig,ax=plt.subplots()
 move,=plt.plot([],[],'-',color='r',label='Frak')
 point,=plt.plot([],[],'o',color='r')
 circle,=plt.plot([],[],'-',color='r')
-def ne_circle_move(R,t):
-  x1=R*(t-np.sin(t))
-  y1=R*(1-np.cos(t))
+line,=plt.plot([],[],'-',color='r')
+def ne_circle_move(R,i):
+  t=np.linspace(0,4*np.pi,500)
+  x1=R*(t[i]-np.sin(t[i]))
+  y1=R*(1-np.cos(t[i]))
   return x1,y1
-def circle_move(R,vx0,vy0,t):
-  x0=vx0*t
-  y0=vy0*t
+def circle_move(R,vx0,vy0,i):
+  t=np.linspace(0,4*np.pi,500)
+  y0=R
   alpha = np.arange(0, 2*np.pi, 0.1)
-  x2=x0+R*np.cos(alpha)
+  x2=t[i]*R+R*np.cos(alpha)
   y2=y0+R*np.sin(alpha)
   return x2,y2
 xdata,ydata=[],[]
+xline,yline=[],[]
 edge=20
 plt.axis('equal')
-ax.set_xlim(-2*edge,2*edge)
+ax.set_xlim(-4*edge,6*edge)
 ax.set_ylim(-edge,edge)
 
 def update(i):
-  xdata.append(ne_circle_move(R=2,t=i)[0])
-  ydata.append(ne_circle_move(R=2,t=i)[1])
+  xdata.append(ne_circle_move(R=4,i=i)[0])
+  ydata.append(ne_circle_move(R=4,i=i)[1])
   move.set_data(xdata,ydata)
-  point.set_data(ne_circle_move(R=2,t=i)[0],ne_circle_move(R=2,t=i)[1])
-  circle.set_data(circle_move(R=4.2,vx0=2,vy0=0,t=i))
+  point.set_data(ne_circle_move(R=4,i=i)[0],ne_circle_move(R=4,i=i)[1])
+  circle.set_data(circle_move(R=4,vx0=1.6,vy0=0,i=i))
+  xline.append()
+  yline.append()
+  line.set_data(xline,yline)
 ani = FuncAnimation(fig,
 update,
-frames=np.arange(0,4*np.pi,0.1),
+frames=500,
 interval=30
 )
 ani.save('lab_7_task_1_DOP.gif')
