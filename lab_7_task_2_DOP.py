0ad8c7d469bdc0e816bbf183c8d388044abb7548
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-01-16 12:07:16 +0300

    Update lab_7_task_2_DOP.py

--- lab_7_task_2_DOP.py
+++ lab_7_task_2_DOP.py
@@ -3,22 +3,25 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 fig,ax=plt.subplots()
-move,=plt.plot([],[],'o',color='r',label='Frak')
-
-def sin_move(vx0,vy0,A,f,t,time):
-  time=np.arange(0,10,0.1)
-  y0=vy0*time
-  x=np.arange(0,10,0.1)
-  y=y0+A*np.sin(f*t)
-  return x,y
-plt.axis('equal')
-ax.set_xlim(-3,3)
-ax.set_ylim(-3,3)
-
-def animate(i):
-  move.set_data(sin_move(vx0=0.01,vy0=0.01,time=i,A=30,f=10,t=20))
-ani=FuncAnimation(fig,
-animate,
+move1,=plt.plot([],[],'-',color='r',label='Frak')
+move2,=plt.plot([],[],'-',color='g',label='Frak')
+xdata,ydata=[],[]
+t=np.arange(0,2*np.pi,0.1)
+ax.set_xlim(0,2*np.pi)
+ax.set_ylim(-1,1)
+x1,y1=[],[]
+x2,y2=[],[]
+def update(frame,i=t):
+  t1=1*i
+  t2=0.8*i
+  x1.append(t1)
+  y1.append(np.sin(t1)
+  x2.append(t2)
+  y2.append(np.sin(t2))
+  move1.set_data(x1,y1)
+  move2.set_data(x2,y2)
+ani = FuncAnimation(fig, 
+update, 
 frames=100,
 interval=30
 )
