e5b31ab00c5381e3436e165a05ae5e00f8c535f1
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-05 22:13:13 +0300

    Update lab_9_task_4.py

--- lab_9_task_4.py
+++ lab_9_task_4.py
@@ -1,1 +1,21 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
+t = np.arange(0, 1000, 10)
+
+
+def radio_function(v, t):
+    F = b - k * v
+    dNdt = F / m
+    return dNdt
+
+
+b = 1
+k = 2
+m = 138
+v_0 = 30 * t
+solve_Bi = odeint(radio_function, v_0, t)
+
+plt.plot(t, solve_Bi[:, 0])
+plt.show()
