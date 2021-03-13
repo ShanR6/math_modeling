91508c01835b475d5863fa02ecabc0191efb594f
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-13 14:19:13 +0200

    Update lab_9_task_3_DOP.py

--- lab_9_task_3_DOP.py
+++ lab_9_task_3_DOP.py
@@ -2,19 +2,20 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-t = np.arange(0, 24, 1)
+t = np.arange(0, 365, 0.1)
 
 
-def radio_function(S, t):
-    dvdt = R * ((S * np.cos(ugol)) * gg)
-    return dvdt
+def radio_function(w, t):
+  dWdt = (R**2 * L) / (a * w)
+  return dWdt
 
-
+w_0 = 0
+w = (2 * np.pi)/365
 L = 3.827 * 10 ** 26
-q = 147 * 10 ** 6
+a = 147 * 10 ** 6
 v = 30.4
 R = 6400
-solve_Bi = odeint(radio_function, S_0, t)
+solve_Bi = odeint(radio_function, w_0, t)
 
 plt.plot(t, solve_Bi[:, 0])
 plt.show()
