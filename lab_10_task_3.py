e010b0094ac4b98f710d8f66ff90e8b835b6fef3
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-26 13:05:51 +0300

    Update lab_10_task_3.py

--- lab_10_task_3.py
+++ lab_10_task_3.py
@@ -6,7 +6,7 @@ x = np.arange(-5, 5, 0.1)
 
 
 def diff_func(z, x):
-    w, t = z
+    w, y = z
 
     dy_dt = w
     dw_dt = np.sin(x) + np.cos(x)
@@ -14,9 +14,9 @@ def diff_func(z, x):
     return dy_dt, dw_dt
 
 
-t0 = 1
-w0 = 3/t0
-z0 = w0, t0
+y0 = 3
+w0 = 0
+z0 = w0, y0
 
 sol = odeint(diff_func, z0, x)
 
