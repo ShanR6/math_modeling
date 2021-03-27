8a60f0076c15aa6232f1f52f803f0b456647f18c
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-27 14:25:48 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,44 +9,49 @@ t = np.linspace(0, 5, frames)
 
 # Определяем функцию для системы диф. уравнений
 def move_func(z, t):
-    y, v_y = z
+    x, v_x, y, v_y = z
+    dxdt = v_x
+    dv_xdt = 0
     dydt = v_y
     dv_ydt = - g * m - p * m * v
-    return dydt, dv_ydt
+    return dxdt, dv_xdt, dydt, dv_ydt
 # Определяем начальные значения и параметры
 g = 9.8
 v = 20
 m = 0.5 
 p = 0.1 / 0.5
 
+x0 = 0
+v_x0 = 0
 y0 = 0  
 v_y0 = v
 
-z0 = y0, v_y0
+z0 = x0, v_x0, y0, v_y0
 # Решаем систему диф. уравнений
 def solve_func(i, key):
     sol = odeint(move_func, z0, t)
     if key == 'point':
+        x = sol[i, 0]
         y = sol[i, 2]
     else:
+        x = sol[:i, 0]
         y = sol[:i, 2]
-    return y
+    return x, y
   
   # Строим решение в виде графика и анимируем
 fig, ax = plt.subplots()
 
 ball, = plt.plot([], [], 'o', color='r')
-ball_line, = plt.plot([], [], '-', color='r')
 def animate(i):
     ball.set_data(solve_func(i, 'point'))
 
 ani = FuncAnimation(fig,
                     animate,
                     frames=frames,
-                    interval=30)
+                    interval=5)
 
 edge = 30
-ax.set_xlim(0, edge)
-ax.set_ylim(0, edge)
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
 
 plt.show()
