bd8dc9a7d46941f27ebc8e3f55edaea369c43d63
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-27 11:47:50 +0300

    Create lab_10_task_2_DOP.py

---
+++ lab_10_task_2_DOP.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 5, 0.1)
+
+
+def diff_func(z, x):
+    w, y = z
+    dy_dx = w
+    dw_dx = (-3*y**2)/(np.sqrt(x)*(y-x/w))
+    return dy_dx, dw_dx
+
+
+y0 = 0
+w0 = 1
+z0 = w0, y0
+sol = odeint(diff_func, z0, x)
+
+plt.plot(x, sol[:, 0], 'b')
+plt.plot(x, sol[:, 1], 'r')
+plt.show()
