297235afe49a150c3ed0ef8d341f8557f7b2dee0
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-27 14:28:23 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,11 +3,11 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Определяем переменную величину и количество кадров
-frames = 200
+
+frames = 100
 t = np.linspace(0, 5, frames)
 
-# Определяем функцию для системы диф. уравнений
+
 def move_func(z, t):
     x, v_x, y, v_y = z
     dxdt = v_x
@@ -15,7 +15,7 @@ def move_func(z, t):
     dydt = v_y
     dv_ydt = - g * m - p * m * v
     return dxdt, dv_xdt, dydt, dv_ydt
-# Определяем начальные значения и параметры
+
 g = 9.8
 v = 20
 m = 0.5 
@@ -27,18 +27,14 @@ y0 = 0
 v_y0 = v
 
 z0 = x0, v_x0, y0, v_y0
-# Решаем систему диф. уравнений
+
 def solve_func(i, key):
     sol = odeint(move_func, z0, t)
     if key == 'point':
         x = sol[i, 0]
         y = sol[i, 2]
-    else:
-        x = sol[:i, 0]
-        y = sol[:i, 2]
     return x, y
   
-  # Строим решение в виде графика и анимируем
 fig, ax = plt.subplots()
 
 ball, = plt.plot([], [], 'o', color='r')
@@ -48,7 +44,7 @@ def animate(i):
 ani = FuncAnimation(fig,
                     animate,
                     frames=frames,
-                    interval=5)
+                    interval=30)
 
 edge = 30
 ax.set_xlim(-edge, edge)
