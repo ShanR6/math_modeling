3695a2c0f39f6ebbd9fbaf6089a6d067a3ce729b
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-27 13:38:38 +0200

    Update lab_10_task_4_DOP.py

--- lab_10_task_4_DOP.py
+++ lab_10_task_4_DOP.py
@@ -2,21 +2,21 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-y = np.arange(-1, 1, 0.1)
+x = np.arange(-1, 1, 0.1)
 
 
 def diff_func(z, y):
-    w, x = z
+    y, w = z
     dy_dx = w
-    dw_dx = ((4 * x ** 2 + 1 / 2) * y - x * w) / x ** 2
+    dw_dx = ((4 * x ** 2 + 1) * y - x * w) / x ** 2
     return dy_dx, dw_dx
 
 
-x0 = 3
-w0 = 0
-z0 = w0, x0
-sol = odeint(diff_func, z0, y)
+y0 = 3
+w0 = 0.1
+z0 = y0, w0
+sol = odeint(diff_func, z0, x)
 
-plt.plot(y, sol[:, 0], 'b')
-plt.plot(y, sol[:, 1], 'r')
+plt.plot(x, sol[:, 0], 'b')
+plt.plot(x, sol[:, 1], 'r')
 plt.show()
