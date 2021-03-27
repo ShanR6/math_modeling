3b317c6a3741e881a8ceccfb9fa8138adf5a88e1
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-03-27 13:34:04 +0200

    Update lab_10_task_3_DOP.py

--- lab_10_task_3_DOP.py
+++ lab_10_task_3_DOP.py
@@ -6,15 +6,15 @@ t = np.arange(0, 3, 0.1)
 
 
 def diff_func(z, t):
-    w, y = z
+    y, w = z
     dy_dt = w
     dw_dt = np.sqrt(1 - w ** 2)
     return dy_dt, dw_dt
 
 
-y0 = 1  
+y0 = 0.1
 w0 = 0
-z0 = w0, y0
+z0 = y0, w0
 sol = odeint(diff_func, z0, t)
 
 plt.plot(t, sol[:, 0], 'b')
