ea9c2aa6b2c5234b56937d45ebe9ff309b4c4944
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-04-16 10:57:34 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,7 @@
-
+f = 0
+count = 0
+for x in range(2320, 10988):
+  if (x % 2 == 0 or x % 7 == 0) and x % 11 != 0 and x % 13 != 0 and x % 17 != 0 and x % 19 != 0:
+    f = x
+    count += 1
+print(count, f)
