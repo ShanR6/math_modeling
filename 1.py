cbf26e2e05430dc032070722cfc03374651eb9c9
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2021-04-22 10:08:05 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,12 @@
-def dell(n, m):
-  return n % m == 0
-
 A = 1
 while True:
-  found = False
-  for x in range(1, 10**6):
-    if not(((not dell(x, A)) and dell(x, 21)) <= dell(x, 14)):
-      found = True
-      break
-  if not found:
+  for x in range(1, 1000):
+    for y in range(1, 1000):
+      if not((7 * y + x < A) or (2 * x + 3 * y > 98)):
+        break
+    else:
+      continue
+    break
+  else:
     print(A)
   A += 1
